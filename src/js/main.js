536a72020a06570a3ba3595dde9d46a04980337c
lopeentornos <lopeeeee23@gmail.com>
2025-01-19 13:15:14 +0100

    trabajojuan

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+
+import { getSlider } from 'simple-slider';
+
+getSlider({
+  container: document.getElementById('myslider'),
+  transitionTime:1,
+  delay:3.5
+});
+
+
+
+import AOS from 'aos';
+
+AOS.init({
+  duration: 1200,
+})
+
+
